25cab2c0beb0ec8b22bb1c69770ec6d86d04d564
wizard503 <josemdg12@gmail.com>
2024-04-16 16:48:29 -0600

    Add icon

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'Jose Duran', // Esto se muestra en la pantalla de inicio
+    // ... otras opciones de PWA
+    icon: {
+      src: './assets/JD2.png', // Ruta a tu archivo de icono
+      sizes: '192x192', // Diferentes tamaños para varios dispositivos
+      type: 'image/png' // Formato del archivo de icono
+    }
+  }
 })
